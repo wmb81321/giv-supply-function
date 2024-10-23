e7790ca630969831781f68fffe0ed85d424c3558
mhmdksh <eddsnx3@gmail.com>
2024-10-23 14:34:17 +0400

    Adding Polygon ZKEVM

--- src/coingecko.js
+++ src/coingecko.js
@@ -2,11 +2,15 @@ const ethers = require("ethers");
 const urlxDAIProvider = "https://rpc.gnosischain.com";
 const urlMainetProvider = "https://rpc.ankr.com/eth";
 const urlOptimismProvider = "https://mainnet.optimism.io";
+const urlPolygonzkEVMProvider = "https://rpc.ankr.com/polygon_zkevm";
+
 const TokenArtifact = require("./abi/GIV.json");
 const BridgedTokenArtifact = require("./abi/GIV-Bridged-L2.json");
+
 const Token = "0x900db999074d9277c5da2a43f252d74366230da0";
 const Token_xDAI = "0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75";
 const Token_optimism = '0x528CDc92eAB044E1E39FE43B9514bfdAB4412B98';
+const Token_polygonzkEVM = '0xddafb91475bbf6210a151fa911ac8fda7de46ec2';
 
 const mainnet_sc = [
   "0x87dE995F6744B75bBe0255A973081142aDb61f4d", // Token Distro
@@ -29,14 +33,22 @@ const optimism_sc = [
   "0x0018C6413BFE5430ff9ba4bD7ac3B6AA89BEBD9b", // nrGIV multisig
 ];
 
+const PolygonzkEVM_sc = [
+  '0x4fB9B10ECDe1b048DBC79aBEAB3793edc93a0d54', // Token Distro
+  '0xaF4D68B8084b01D2c8120120497511035A2ea3a1', // Giveth Multisig
+  '0xa1769a425EA1197f32d8C3e0fbE3F92EaDACC21A', // Multisig liquidity
+];
+
 async function calculateTokenSupplyCG() {
   const mainnetProvider = new ethers.providers.JsonRpcProvider(urlMainetProvider);
   const xDAIProvider = new ethers.providers.JsonRpcProvider(urlxDAIProvider);
   const optimismProvider = new ethers.providers.JsonRpcProvider(urlOptimismProvider);
+  const polygonzkEVMProvider = new ethers.providers.JsonRpcProvider(urlPolygonzkEVMProvider);
 
   const token = new ethers.Contract(Token, TokenArtifact.abi, mainnetProvider);
   const token_xdai = new ethers.Contract(Token_xDAI, TokenArtifact.abi, xDAIProvider);
   const token_optimism = new ethers.Contract(Token_optimism, BridgedTokenArtifact, optimismProvider);
+  const token_polygonzkEVM = new ethers.Contract(Token_polygonzkEVM, BridgedTokenArtifact, polygonzkEVMProvider);
 
   const totalSupply = await token.totalSupply();
   let circulating = totalSupply;
@@ -44,6 +56,7 @@ async function calculateTokenSupplyCG() {
   const mainnet_sc_promises = mainnet_sc.map(item => token.balanceOf(item));
   const xdai_sc_promises = xdai_sc.map(item => token_xdai.balanceOf(item));
   const optimism_sc_promises = optimism_sc.map(item => token_optimism.balanceOf(item));
+  const polygonzkEVM_sc_promises = PolygonzkEVM_sc.map(item => token_polygonzkEVM.balanceOf(item));
 
   const mainnet_values = await Promise.all(mainnet_sc_promises);
   mainnet_values.forEach(value => {
@@ -57,6 +70,11 @@ async function calculateTokenSupplyCG() {
 
   const optimism_values = await Promise.all(optimism_sc_promises);
   optimism_values.forEach(value => {
+    circulating = circulating.sub(value);
+  });
+
+  const polygonzkEVM_values = await Promise.all(polygonzkEVM_sc_promises);
+  polygonzkEVM_values.forEach(value => {
     circulating = circulating.sub(value);
   });
 
--- src/coinmarketcap.js
+++ src/coinmarketcap.js
@@ -3,6 +3,7 @@ const ethers = require("ethers");
 const urlxDAIProvider = "https://rpc.gnosischain.com";
 const urlMainetProvider = "https://rpc.ankr.com/eth";
 const urlOptimismProvider = "https://mainnet.optimism.io";
+const urlPolygonzkEVMProvider = "https://rpc.ankr.com/polygon_zkevm";
 // ABI files
 const TokenArtifact = require("./abi/GIV.json");
 const BridgedTokenArtifact = require("./abi/GIV-Bridged-L2.json");
@@ -10,6 +11,7 @@ const BridgedTokenArtifact = require("./abi/GIV-Bridged-L2.json");
 const Token = "0x900db999074d9277c5da2a43f252d74366230da0";
 const Token_xDAI = "0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75";
 const Token_optimism = '0x528CDc92eAB044E1E39FE43B9514bfdAB4412B98';
+const Token_polygonzkEVM = '0xddafb91475bbf6210a151fa911ac8fda7de46ec2';
 
 // Smart contract addresses to exclude from circulating supply
 const mainnet_sc = [
@@ -33,15 +35,23 @@ const optimism_sc = [
     "0x0018C6413BFE5430ff9ba4bD7ac3B6AA89BEBD9b", // nrGIV multisig
 ];
 
+const PolygonzkEVM_sc = [
+    '0x4fB9B10ECDe1b048DBC79aBEAB3793edc93a0d54', // Token Distro
+    '0xaF4D68B8084b01D2c8120120497511035A2ea3a1', // Giveth Multisig
+    '0xa1769a425EA1197f32d8C3e0fbE3F92EaDACC21A', // Multisig liquidity
+  ];  
+
 // Function to calculate token supply
 async function calculateTokenSupplyCMC(query) {
     const mainnetProvider = new ethers.providers.JsonRpcProvider(urlMainetProvider);
     const xDAIProvider = new ethers.providers.JsonRpcProvider(urlxDAIProvider);
     const optimismProvider = new ethers.providers.JsonRpcProvider(urlOptimismProvider);
+    const polygonzkEVMProvider = new ethers.providers.JsonRpcProvider(urlPolygonzkEVMProvider);
 
     const token = new ethers.Contract(Token, TokenArtifact.abi, mainnetProvider); // Use .abi for GIV.json
     const token_xdai = new ethers.Contract(Token_xDAI, TokenArtifact.abi, xDAIProvider); // Use .abi for GIV.json
     const token_optimism = new ethers.Contract(Token_optimism, BridgedTokenArtifact, optimismProvider); // Use the array directly for GIV-Bridged-L2.json
+    const token_polygonzkEVM = new ethers.Contract(Token_polygonzkEVM, BridgedTokenArtifact, polygonzkEVMProvider); // Use the array directly for GIV-Bridged-L2.json
 
     const totalSupply = await token.totalSupply();
     let circulating_supply = totalSupply;
@@ -49,6 +59,7 @@ async function calculateTokenSupplyCMC(query) {
     const mainnet_sc_promises = mainnet_sc.map(item => token.balanceOf(item));
     const xdai_sc_promises = xdai_sc.map(item => token_xdai.balanceOf(item));
     const optimism_sc_promises = optimism_sc.map(item => token_optimism.balanceOf(item));
+    const polygonzkEVM_sc_promises = PolygonzkEVM_sc.map(item => token_polygonzkEVM.balanceOf(item));
 
     const mainnet_values = await Promise.all(mainnet_sc_promises);
     mainnet_values.forEach(value => {
@@ -63,6 +74,11 @@ async function calculateTokenSupplyCMC(query) {
     const optimism_values = await Promise.all(optimism_sc_promises);
     optimism_values.forEach(value => {
         circulating_supply = circulating_supply.sub(value);
+    });
+
+    const polygonzkEVM_values = await Promise.all(polygonzkEVM_sc_promises);
+    polygonzkEVM_values.forEach(value => {
+      circulating_supply = circulating_supply.sub(value);
     });
 
     if (query === "totalcoins") {
